879fcc6c595df88a2d919a15fa32e36bd0b23e96
v.cheryasova <v.cheryasova@cleack.com>
2019-02-22 18:29:01 +0200

    add resources'

---
+++ gulp/tasks/sass.js
@@ -0,0 +1,24 @@
+module.exports = function() {
+
+$.gp.sass.compiler = require('node-sass');
+$.gulp.task('sass', function () {
+	return $.gulp.src('src/static/sass/main.sass')
+	.pipe($.gp.sass({
+      // includePaths: require('node-normalize-scss').with('other/path', 'another/path')
+      // - or -
+      includePaths: require('node-normalize-scss').includePaths
+    }))
+	.pipe($.gp.sourcemaps.init())
+	.pipe($.gp.sass({
+		'include css': true
+	}))
+	.pipe($.gp.autoprefixer({
+        browsers: ['last 10 versions'],
+        cascade: false
+    }))
+    .pipe($.gp.sass({outputStyle: 'compressed'}).on('error', $.gp.sass.logError))
+	.pipe($.gp.csso())
+	.pipe($.gp.sourcemaps.write())
+	.pipe($.gulp.dest('build/static/css'));
+});
+}
